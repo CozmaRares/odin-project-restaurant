171bd9d83e0b9dd0f6fd3c5f9fd698a8a4bc0450
Cozma Rares <cozmarares11@gmail.com>
2022-09-15 22:06:34 +0300

    add header scroll behaviour

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import "./styles.css";
 import "./main-page.css";
+
+const observer = new IntersectionObserver(entries =>
+  entries.forEach(entry =>
+    entry.isIntersecting
+      ? entry.target.classList.add("show")
+      : entry.target.classList.remove("show")
+  )
+);
+
+observer.observe(document.querySelector("footer"));
