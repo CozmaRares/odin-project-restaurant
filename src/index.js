563b6dd75c0229d36ded1c8397762c0b10191335
Cozma Rares <cozmarares11@gmail.com>
2022-09-16 00:01:27 +0300

    add menu section

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import "./styles.css";
 import "./main-page.css";
 
 const observer = new IntersectionObserver(entries =>
-  entries.forEach(entry =>
+  entries.forEach(entry => {
+    console.log(entry.target);
+
     entry.isIntersecting
       ? entry.target.classList.add("show")
-      : entry.target.classList.remove("show")
-  )
+      : entry.target.classList.remove("show");
+  })
 );
 
 observer.observe(document.querySelector("footer"));
